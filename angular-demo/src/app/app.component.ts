5c3912ae947cd096f37a6a6f17d503603efe009d
Cory Rylan <splintercode.cb@gmail.com>
2019-09-27 16:24:46 -0500

    fix: simplify angular example

--- angular-demo/src/app/app.component.ts
+++ angular-demo/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, filter, tap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
@@ -18,15 +18,11 @@ interface YouTubeResult {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  searchForm: FormGroup;
+  search = new FormControl('');
   results: Observable<{}[]>;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
-    this.searchForm = this.formBuilder.group({
-      search: ['', Validators.required]
-    });
-
-    this.results = this.searchForm.controls.search.valueChanges.pipe( // Observable Form
+  constructor(private http: HttpClient) {
+    this.results = this.search.valueChanges.pipe( // Observable Form
       filter(value => value.length > 2),
       debounceTime(500),
       distinctUntilChanged(),
